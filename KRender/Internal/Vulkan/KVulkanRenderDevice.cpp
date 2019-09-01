6de85ba2dc81cfec62cdfe968f4c45bedaa93e36
King19931229 <562538332@qq.com>
2019-09-01 14:42:19 +0800

    Vulkan设备初始化

--- KRender/Internal/Vulkan/KVulkanRenderDevice.cpp
+++ KRender/Internal/Vulkan/KVulkanRenderDevice.cpp
@@ -1,15 +1,81 @@
 #include "KVulkanRenderDevice.h"
+#include "KVulkanHelper.h"
+
+#include <algorithm>
+#include <functional>
 #include <assert.h>
 
 const char* VALIDATION_LAYER[] =
 {
 	"VK_LAYER_KHRONOS_validation"
 };
- 
+#define VALIDATION_LAYER_COUNT (sizeof(VALIDATION_LAYER) / sizeof(const char*))
+
+//-------------------- Extensions --------------------//
+std::vector<const char*> PopulateExtensions(bool bEnableValidationLayer)
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if (bEnableValidationLayer)
+	{
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+
+	return std::move(extensions);
+}
+
+//-------------------- Validation Layer --------------------//
+VkResult CreateDebugUtilsMessengerEXT(
+	VkInstance instance,
+	const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+	const VkAllocationCallbacks* pAllocator,
+	VkDebugUtilsMessengerEXT* pDebugMessenger)
+{
+	PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr)
+	{
+		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void DestroyDebugUtilsMessengerEXT(
+	VkInstance instance,
+	VkDebugUtilsMessengerEXT debugMessenger,
+	const VkAllocationCallbacks* pAllocator)
+{
+	PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr)
+	{
+		func(instance, debugMessenger, pAllocator);
+	}
+}
+
+void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo, PFN_vkDebugUtilsMessengerCallbackEXT pCallBack)
+{
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	createInfo.pfnUserCallback = pCallBack;
+} 
+
+//-------------------- KVulkanRenderDevice --------------------//
 KVulkanRenderDevice::KVulkanRenderDevice()
-	: mEnableValidationLayer(true)
+	: m_EnableValidationLayer(true)
 {
 	memset(&m_Instance, 0, sizeof(m_Instance));
+	memset(&m_Device, 0, sizeof(m_Device));
+	memset(&m_DebugMessenger, 0, sizeof(m_DebugMessenger));
+	memset(&m_PhysicalDevice, 0, sizeof(m_PhysicalDevice));
 }
 
 KVulkanRenderDevice::~KVulkanRenderDevice()
@@ -44,20 +110,199 @@ bool KVulkanRenderDevice::CheckValidationLayerAvailable()
 	return true;
 }
 
-std::vector<const char*> KVulkanRenderDevice::PrepareExtensions()
+KVulkanRenderDevice::QueueFamilyIndices KVulkanRenderDevice::FindQueueFamilies(VkPhysicalDevice vkDevice)
 {
-	uint32_t glfwExtensionCount = 0;
-	const char** glfwExtensions;
-	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	QueueFamilyIndices familyIndices = {};
 
-	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(vkDevice, &queueFamilyCount, nullptr);
 
-	if (mEnableValidationLayer)
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(vkDevice, &queueFamilyCount, queueFamilies.data());
+
+	familyIndices.graphicsFamily = -1;
+	familyIndices.graphicsFamilyFound = false;
+
+	int idx = -1;
+	for (const auto& queueFamily : queueFamilies)
 	{
-		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+		++idx;
+		if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			familyIndices.graphicsFamily = idx;
+			familyIndices.graphicsFamilyFound = true;
+			break;
+		}
 	}
 
-	return std::move(extensions);
+	return std::move(familyIndices);
+}
+
+KVulkanRenderDevice::PhysicalDevice KVulkanRenderDevice::GetPhysicalDeviceProperty(VkPhysicalDevice vkDevice)
+{
+	PhysicalDevice device;
+	
+	// First part
+	device.device = vkDevice;
+
+	vkGetPhysicalDeviceProperties(vkDevice, &device.deviceProperties);
+	vkGetPhysicalDeviceFeatures(vkDevice, &device.deviceFeatures);
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(vkDevice, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(vkDevice, &queueFamilyCount, queueFamilies.data());
+
+	QueueFamilyIndices familyIndices = FindQueueFamilies(vkDevice);
+
+	// Second part
+	device.suitable = familyIndices.graphicsFamilyFound;
+
+	if(device.suitable)
+	{
+		device.score = 0;
+
+		// Discrete GPUs have a significant performance advantage
+		if (device.deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+		{
+			device.score += 1000;
+		}
+
+		// Maximum possible size of textures affects graphics quality
+		device.score += device.deviceProperties.limits.maxImageDimension2D;
+	}
+	else
+	{
+		device.score = -100;
+	}
+
+	return std::move(device);
+}
+
+bool KVulkanRenderDevice::PickPhysicsDevice()
+{
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(m_Instance, &deviceCount, nullptr);
+	if (deviceCount > 0)
+	{
+		std::vector<VkPhysicalDevice> vkDevices(deviceCount);
+		vkEnumeratePhysicalDevices(m_Instance, &deviceCount, vkDevices.data());
+
+		std::vector<PhysicalDevice> devices;
+
+		std::for_each(vkDevices.begin(), vkDevices.end(), [&](VkPhysicalDevice& device)
+		{
+			devices.push_back(GetPhysicalDeviceProperty(device));
+		});
+
+		for(auto it = devices.begin(); it != devices.end();)
+		{
+			if(it->suitable)
+			{
+				++it;
+			}
+			else
+			{
+				it = devices.erase(it);
+			}
+		}
+
+		if(devices.size() > 0)
+		{
+			std::sort(devices.begin(), devices.end(), [&](PhysicalDevice& lhs, PhysicalDevice rhs)->bool
+			{
+				return lhs.score < rhs.score;
+			});
+
+			m_PhysicalDevice = devices[0];
+			return true;
+		}
+	}
+	return false;
+}
+
+bool KVulkanRenderDevice::CreateLogicalDevice()
+{
+	QueueFamilyIndices indices = FindQueueFamilies(m_PhysicalDevice.device);
+
+	if(indices.graphicsFamilyFound)
+	{
+		// 填充VkDeviceQueueCreateInfo
+		VkDeviceQueueCreateInfo queueCreateInfo = {};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily;
+		queueCreateInfo.queueCount = 1;
+
+		float queuePriority = 1.0f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		// 填充VkDeviceCreateInfo
+		VkPhysicalDeviceFeatures deviceFeatures = {};
+		VkDeviceCreateInfo createInfo = {};
+
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+
+		createInfo.pEnabledFeatures = &deviceFeatures;
+
+		// 尽管最新Vulkan实例验证层与设备验证层已经统一
+		// 但是最好保留代码兼容性
+		if (m_EnableValidationLayer)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(VALIDATION_LAYER_COUNT);
+			createInfo.ppEnabledLayerNames = VALIDATION_LAYER;
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
+
+		if (vkCreateDevice(m_PhysicalDevice.device, &createInfo, nullptr, &m_Device) == VK_SUCCESS)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+VkBool32 KVulkanRenderDevice::DebugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData
+	)
+{
+	printf("[Vulkan Validation Layer]: %s\n", pCallbackData->pMessage);
+	return VK_FALSE;
+}
+
+bool KVulkanRenderDevice::SetupDebugMessenger()
+{
+	if(m_EnableValidationLayer)
+	{
+		VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+		PopulateDebugMessengerCreateInfo(createInfo, DebugCallback);
+
+		if (CreateDebugUtilsMessengerEXT(m_Instance, &createInfo, nullptr, &m_DebugMessenger) == VK_SUCCESS)
+		{
+			return true;
+		}
+		return false;
+	}
+	return true;
+}
+
+bool KVulkanRenderDevice::UnsetDebugMessenger()
+{
+	if(m_EnableValidationLayer)
+	{
+		DestroyDebugUtilsMessengerEXT(m_Instance, m_DebugMessenger, nullptr);
+	}
+	return true;
 }
 
 bool KVulkanRenderDevice::Init()
@@ -76,28 +321,40 @@ bool KVulkanRenderDevice::Init()
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 
+	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = {};
 	// 挂接验证层
-	if(mEnableValidationLayer)
+	if(m_EnableValidationLayer)
 	{
 		bool bCheckResult = CheckValidationLayerAvailable();
 		assert(bCheckResult);
-		createInfo.enabledLayerCount = sizeof(VALIDATION_LAYER) / sizeof(VALIDATION_LAYER[0]);
+		createInfo.enabledLayerCount = VALIDATION_LAYER_COUNT;
 		createInfo.ppEnabledLayerNames = VALIDATION_LAYER;
+
+		// 这是为了检查vkCreateInstance与SetupDebugMessenger之间的错误
+		PopulateDebugMessengerCreateInfo(debugCreateInfo, DebugCallback);
+		createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
 	}
 	else
 	{
 		createInfo.enabledLayerCount = 0;
+		createInfo.pNext = nullptr;
 	}
 
 	// 挂接扩展
-	auto extensions = PrepareExtensions();
+	auto extensions = PopulateExtensions(m_EnableValidationLayer);
 	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
 	createInfo.ppEnabledExtensionNames = extensions.data();
 
 	if (vkCreateInstance(&createInfo, nullptr, &m_Instance) == VK_SUCCESS)
 	{
-		if(PostInit())
-			return true;
+		if(!SetupDebugMessenger())
+			return false;
+		if(!PickPhysicsDevice())
+			return false;
+		if(!CreateLogicalDevice())
+			return false;
+		PostInit();
+		return true;
 	}
 	else
 	{
@@ -108,11 +365,13 @@ bool KVulkanRenderDevice::Init()
 
 bool KVulkanRenderDevice::UnInit()
 {
+	UnsetDebugMessenger();
+	vkDestroyDevice(m_Device, nullptr);
 	vkDestroyInstance(m_Instance, nullptr);
 	return true;
 }
 
-bool KVulkanRenderDevice::PostInit()
+bool KVulkanRenderDevice::RecordExtentions()
 {
 	m_Extentions.clear();
 	uint32_t extensionCount = 0;
@@ -129,11 +388,20 @@ bool KVulkanRenderDevice::PostInit()
 			return true;
 		}
 	}
-	return false;	
+	return false;
 }
 
 bool KVulkanRenderDevice::QueryExtensions(DeviceExtensions& exts)
 {
 	exts = m_Extentions;
 	return true;
+}
+
+bool KVulkanRenderDevice::PostInit()
+{
+	if(RecordExtentions())
+	{
+		return true;
+	}
+	return false;
 }
--- KRender/Internal/Vulkan/KVulkanRenderDevice.h
+++ KRender/Internal/Vulkan/KVulkanRenderDevice.h
@@ -5,15 +5,50 @@
 
 class KVulkanRenderDevice : IKRenderDevice
 {
-	VkInstance m_Instance;
-	DeviceExtensions m_Extentions;
+	struct QueueFamilyIndices
+	{
+		uint32_t graphicsFamily;
+		bool graphicsFamilyFound;
+	};
 
-	bool mEnableValidationLayer;
+	struct PhysicalDevice
+	{
+		VkPhysicalDevice device;
+		VkPhysicalDeviceProperties deviceProperties;
+		VkPhysicalDeviceFeatures deviceFeatures;
+
+		bool suitable;
+		int score;
+	};
+protected:
+	VkInstance m_Instance;
+	VkDevice m_Device;
+	VkDebugUtilsMessengerEXT m_DebugMessenger;
+
+	PhysicalDevice m_PhysicalDevice;
+	DeviceExtensions m_Extentions;
+	bool m_EnableValidationLayer;
 
 	bool CheckValidationLayerAvailable();
-	std::vector<const char*> PrepareExtensions();
+	bool SetupDebugMessenger();
+	bool UnsetDebugMessenger();
+
+	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+	PhysicalDevice GetPhysicalDeviceProperty(VkPhysicalDevice device);
+
+	bool PickPhysicsDevice();
+	bool CreateLogicalDevice();
+	bool RecordExtentions();
 
 	bool PostInit();
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData
+		);
+
 public:
 	KVulkanRenderDevice();
 	virtual ~KVulkanRenderDevice();
