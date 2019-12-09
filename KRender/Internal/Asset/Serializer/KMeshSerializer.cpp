d9254775d08309b64872c598f146e0ee287f294c
King19931229 <562538332@qq.com>
2019-12-09 23:16:51 +0800

    补传文件

---
+++ KRender/Internal/Asset/Serializer/KMeshSerializer.cpp
@@ -0,0 +1,43 @@
+#include "KMeshSerializer.h"
+#include "KMeshSerializerV0.h"
+#include "KBase/Interface/IKDataStream.h"
+
+namespace KMeshSerializer
+{
+	bool LoadFromFile(IKRenderDevice* device, KMesh* pMesh, const char* path, size_t frameInFlight, size_t renderThreadNum)
+	{
+		IKDataStreamPtr pData = GetDataStream(IT_FILEHANDLE);
+		if(pData && pData->Open(path, IM_READ))
+		{
+			// no need to judge version now
+			KMeshSerializerV0 reader(device);
+			bool bRet = reader.LoadFromStream(pMesh, pData, frameInFlight, renderThreadNum);
+			pData->Close();
+			return bRet;
+		}
+		return false;
+	}
+
+	bool SaveAsFile(KMesh* pMesh, const char* path, MeshSerializerVersion version)
+	{
+		IKDataStreamPtr pData = GetDataStream(IT_FILEHANDLE);
+		if(pData && pData->Open(path, IM_WRITE))
+		{
+			bool bRet = false;
+			switch (version)
+			{
+			case MSV_VERSION_0_0:
+				{
+					KMeshSerializerV0 writer(nullptr);
+					bRet = writer.SaveToStream(pMesh, pData);
+				}
+				break;
+			default:
+				break;
+			}
+			pData->Close();
+			return bRet;
+		}
+		return false;
+	}
+}
